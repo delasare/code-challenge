0446615c6fddf9e45e06d0043c3265b2bcd1b988
Ron de las Alas <ron.delasalas@gmail.com>
2021-06-21 09:50:34 -0400

    working

--- src/count-context.js
+++ src/count-context.js
@@ -1,16 +1,14 @@
 import * as React from 'react';
-import ReactDOM from 'react-dom';
-
 const CountContext = React.createContext();
 
 
 function countReducer(state, action) {
     switch (action.type) {
         case 'increment': {
-            return {count: state.count + 1}
+            return { count: state.count + 1 };
         }
         case 'decrement': {
-            return {count: state.count -1 1}
+            return { count: state.count - 1 };
         }
         default: {
             throw new Error(`Unhandled action type: ${action.type}`)
@@ -24,7 +22,14 @@ function CountProvider({ children }) {
     return <CountContext.Provider value={value}>{children}</CountContext.Provider>
 }
 
+function useCount() {
+    const context = React.useContext(CountContext);
+    if (context === undefined) {
+        throw new Error('useCount must be used within a Context Provider');
+    }
+    return context;
+}
 
-ReactDOM.render(<CountDisplay />, document.getElementById('root'))
 
-export { CountProvider };
+
+export { CountProvider, useCount };
---
+++ src/count.js
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import { useCount } from './count-context';
+
+
+function CountDisplay() {
+    const { state: { count }, } = useCount();
+    return <div>{`The current count is ${count}`}</div>
+}
+
+
+function Counter() {
+    const { dispatch } = useCount();
+    return (
+        <>
+            <button onClick={() => { dispatch({ type: 'increment' }) }}>
+                Increment Count;
+            </button>
+            <button onClick={() => { dispatch({ type: 'decrement' }) }}>
+                Decrement Count;
+            </button>
+        </>
+    ) 
+}
+
+export {CountDisplay, Counter}
